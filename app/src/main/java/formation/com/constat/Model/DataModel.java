e399d7458cdb5ace1ed81b681717592ff0b0d54b
Mohamed Sami Mejri <medsamimejri@gmail.com>
2018-03-22 20:49:20 +0100

    Add params to DataModel and make the constructor and getter and setter methods

--- app/src/main/java/formation/com/constat/Model/DataModel.java
+++ app/src/main/java/formation/com/constat/Model/DataModel.java
@@ -5,4 +5,47 @@ package formation.com.constat.Model;
  */
 
 public class DataModel {
+    private String firstName;
+    private String lastName;
+    private int phoneNumber;
+    private String eMail;
+
+    public DataModel(String firstName, String lastName, int phoneNumber, String eMail) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phoneNumber = phoneNumber;
+        this.eMail = eMail;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(int phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String geteMail() {
+        return eMail;
+    }
+
+    public void seteMail(String eMail) {
+        this.eMail = eMail;
+    }
 }
